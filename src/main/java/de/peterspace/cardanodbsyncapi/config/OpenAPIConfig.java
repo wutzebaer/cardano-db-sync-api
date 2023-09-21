74029e1eb2ae65336f3579045b93bad0d0135bc5
peter <petergrossmann@email.de>
2023-09-21 16:44:34 +0200

    swagger config

--- src/main/java/de/peterspace/cardanodbsyncapi/config/OpenAPIConfig.java
+++ src/main/java/de/peterspace/cardanodbsyncapi/config/OpenAPIConfig.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Cardano DB-Sync API", version = "1.0"))
+@OpenAPIDefinition(info = @Info(title = "Cardano DB-Sync API", version = "1.0"), servers = { @Server(url = "/", description = "Default Server URL") })
 public class OpenAPIConfig {
 
 }
