8f3a6c8047588f15f14e9a3634cf4ad59a1830ab
Peter Großmann <petergrossmann@email.de>
2026-09-07 14:25:48 +0000

    refactor: update dependencies and enhance post-create script
    Changed the spring-boot-starter-web dependency to spring-boot-starter-webmvc in pom.xml for improved web support. Added maven-compiler-plugin configuration to handle Lombok annotation processing. Updated post-create.sh to include a function for installing SNAPSHOT dependencies from GitHub, enhancing setup automation. Modified import statement in CardanoDbSyncApiApplication.java for correct servlet component scanning.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 # Set ownership of the home directory
 sudo chown -R vscode:vscode /home/vscode
@@ -7,6 +7,20 @@ sudo chown -R vscode:vscode /home/vscode
 # Add ll alias (ls -al)
 echo 'alias ll="ls -al"' >> ~/.bashrc
 echo 'alias mvn=mvnd' >> ~/.bashrc
+
+install_from_github() {
+  set -e
+  local repo="$1"
+  local tmp
+  tmp=$(mktemp -d)
+  git clone --depth 1 "https://github.com/${repo}.git" "$tmp"
+  ./mvnw -f "$tmp/pom.xml" install -DskipTests
+  rm -rf "$tmp"
+}
+
+# SNAPSHOT dependency is not on Maven Central; install it (and its parent) locally
+install_from_github wutzebaer/peterspace-springboot-parent
+install_from_github wutzebaer/cardano-java-lib
 
 # Clean the application
 ./mvnw clean compile
--- src/main/java/de/peterspace/cardanodbsyncapi/CardanoDbSyncApiApplication.java
+++ src/main/java/de/peterspace/cardanodbsyncapi/CardanoDbSyncApiApplication.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
