b83f0d108df99847277edaeaf68e90cb5c85ecd2
Peter Großmann <petergrossmann@email.de>
2026-09-08 08:29:52 +0000

    chore: update release script to include shebang and error handling
    Added a shebang and set the script to exit on error in release.sh. Updated the command to use the local Maven wrapper for building the Docker image, ensuring a more reliable build process.

--- release.sh
+++ release.sh
@@ -1,2 +1,5 @@
-mvnd spring-boot:build-image -Dspring-boot.build-image.imageName=wutzebaer/cardano-db-sync-api:latest -DskipTests=true
+#!/bin/bash
+set -e
+
+./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=wutzebaer/cardano-db-sync-api:latest -DskipTests=true
 docker push wutzebaer/cardano-db-sync-api:latest
