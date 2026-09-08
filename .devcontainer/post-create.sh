fb978ed3d85a591d2e8217fc281f60c0729864c5
Peter Großmann <petergrossmann@email.de>
2026-09-08 06:22:05 +0000

    chore: update Java version and enhance devcontainer setup
    Updated the Java version in pom.xml from 17 to 25 for improved compatibility. Removed the backup versions file as it is no longer needed. Modified the devcontainer configuration to use the new Java image and added Docker-in-Docker support. Simplified the post-create script to directly clone and install the cardano-java-lib dependency, streamlining the setup process. Adjusted error handling configuration in application.yml for better clarity.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,19 +8,11 @@ sudo chown -R vscode:vscode /home/vscode
 echo 'alias ll="ls -al"' >> ~/.bashrc
 echo 'alias mvn=mvnd' >> ~/.bashrc
 
-install_from_github() {
-  set -e
-  local repo="$1"
-  local tmp
-  tmp=$(mktemp -d)
-  git clone --depth 1 "https://github.com/${repo}.git" "$tmp"
-  ./mvnw -f "$tmp/pom.xml" install -DskipTests
-  rm -rf "$tmp"
-}
 
 # SNAPSHOT dependency is not on Maven Central; install it (and its parent) locally
-install_from_github wutzebaer/peterspace-springboot-parent
-install_from_github wutzebaer/cardano-java-lib
+rm -rf /tmp/cardano-java-lib
+git clone --depth 1 "https://github.com/wutzebaer/cardano-java-lib.git" /tmp/cardano-java-lib
+./mvnw -f /tmp/cardano-java-lib/pom.xml install -DskipTests
 
 # Clean the application
 ./mvnw clean compile
